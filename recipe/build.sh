5666bae5cf88be5d9f957fa35206e94718ca61ce
Scott Bailey <sbailey@anaconda.com>
2022-03-01 15:13:12 -0700

    Skip  for  because  is not available.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,17 @@
 mkdir -p Build
 cd Build || exit 1
 
+# Don't build tests for `s390x` since boost is missing.
+if [ "${target_platform}" == 'linux-s390x' ]; then
+    build_tests=OFF
+else
+    build_tests=ON
+fi
+
+
 # Generate the build files.
 cmake .. -G"Ninja" ${CMAKE_ARGS} \
-      -DCLI11_BUILD_TESTS=ON \
+      -DCLI11_BUILD_TESTS=${build_tests} \
       -DCLI11_BUILD_EXAMPLES=OFF \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -17,7 +25,7 @@ ninja install || exit 1
 
 
 # Exit early for s390x since `libboost` is not available.
-if [ "${target_platform}" == 'linux-aarch64' ]; then
+if [ "${target_platform}" == 'linux-s390x' ]; then
     exit 0
 fi
 
