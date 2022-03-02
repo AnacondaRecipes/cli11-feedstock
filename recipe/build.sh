1de005fc9e57e9a2b679d8d3913cec183e441dbe
Scott B <Scott@TheBaileyFamily.org>
2022-03-02 07:51:51 -0700

    Initial fork and update from conda-forge.
    Changes:
    - Initial fork from conda-forge.
    - Update build files:
      - Prefer ninja to NMake/make.
      - Enable author's testing.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,34 @@
-mkdir build
-cd build
+#!/bin/bash
 
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCLI11_BUILD_TESTS=OFF \
-    -DCLI11_BUILD_EXAMPLES=OFF \
-    -DCMAKE_INSTALL_LIBDIR=lib \
-    ${CMAKE_ARGS} \
-    $SRC_DIR
+# Isolate the build.
+mkdir -p Build
+cd Build || exit 1
 
-make install
+# Don't build tests for `s390x` since boost is missing.
+if [ "${target_platform}" == 'linux-s390x' ]; then
+    build_tests=OFF
+else
+    build_tests=ON
+fi
+
+
+# Generate the build files.
+cmake .. -G"Ninja" ${CMAKE_ARGS} \
+      -DCLI11_BUILD_TESTS=${build_tests} \
+      -DCLI11_BUILD_EXAMPLES=OFF \
+      -DCMAKE_PREFIX_PATH=$PREFIX \
+      -DCMAKE_INSTALL_PREFIX=$PREFIX \
+      -DCMAKE_BUILD_TYPE=Release
+
+# Build and install.
+ninja install || exit 1
+
+
+# Exit early for s390x since `libboost` is not available.
+if [ "${target_platform}" == 'linux-s390x' ]; then
+    exit 0
+fi
+
+
+# Perform tests.
+ninja test || exit 1
